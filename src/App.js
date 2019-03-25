839e6315846543f078f67d03e9c49e573567f558
Neil Williams <neil.williams@dvla.gov.uk>
2019-03-25 16:34:34 +0000

    Added socket io and connected to the locally running sensor-service

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { subscribeAccelerometer, subscribeButtonPress } from './socket';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      accelerometerData: {
+        sensorId: '',
+        x: 0,
+        y: 0,
+        z: 0
+      },
+      buttonPressData: ''
+    }
+
+    subscribeAccelerometer((data) => {
+      // {"sensorId":"e96122f9c20c45528b7663f5dbafff95","x":"0.05","y":"-0.03","z":"1.00"}
+      this.setState({
+        accelerometerData: data
+      });
+    });
+
+    subscribeButtonPress((data) => {
+      // e96122f9c20c45528b7663f5dbafff95
+      this.setState({
+        buttonPressData: data
+      });
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p>
+          accelerometerData: <br />
+          x: {this.state.accelerometerData.x} <br />
+          y: {this.state.accelerometerData.y} <br />
+          z: {this.state.accelerometerData.z}
+        </p>
+        <p>
+          buttonPressData: {this.state.buttonPressData}
+        </p>
       </div>
     );
   }
