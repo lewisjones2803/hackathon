91247a8bb3b0323b47fa7a30f5e95bd530a9866c
lewis.jones@dvla.gov.uk <lewis.jones@dvla.gov.uk>
2019-03-26 10:57:35 +0000

    stuffs

--- src/components/TireVis/TireVis.jsx
+++ src/components/TireVis/TireVis.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './TireVis.scss';
+import { Scatter } from 'react-chartjs-2';
+import 'chartjs-plugin-annotation';
+import './our-scatter.js';
 
 class TireVis extends React.Component {
   render() {
-    return(
+    return (
       <div>
-        
+        <script></script>
       </div>
     );
   }
