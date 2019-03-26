94f5698b402de68f0af2d06b1addb53f730a3992
lewis.jones@dvla.gov.uk <lewis.jones@dvla.gov.uk>
2019-03-26 08:17:41 +0000

    created a new DataDisplay component which will contain the code Neil wrote yesterday and App.js imports it

--- src/App.js
+++ src/App.js
@@ -1,51 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import { subscribeAccelerometer, subscribeButtonPress } from './socket';
 import Header from './components/Header/Header';
-import 'bootstrap/dist/css/bootstrap.css'
+import DataDisplay from './components/DataDisplay/DataDisplay';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      accelerometerData: {
-        sensorId: '',
-        x: 0,
-        y: 0,
-        z: 0
-      },
-      buttonPressData: ''
-    }
-
-    subscribeAccelerometer((data) => {
-      // {"sensorId":"e96122f9c20c45528b7663f5dbafff95","x":"0.05","y":"-0.03","z":"1.00"}
-      this.setState({
-        accelerometerData: data
-      });
-    });
-
-    subscribeButtonPress((data) => {
-      // e96122f9c20c45528b7663f5dbafff95
-      this.setState({
-        buttonPressData: data
-      });
-    });
-  }
-
   render() {
     return (
       <div className="App">
         <Header />
-        <p>
-          accelerometerData: <br />
-          x: {this.state.accelerometerData.x} <br />
-          y: {this.state.accelerometerData.y} <br />
-          z: {this.state.accelerometerData.z}
-        </p>
-        <p>
-          buttonPressData: {this.state.buttonPressData}
-        </p>
+        <DataDisplay />
       </div>
     );
   }
---
+++ src/components/DataDisplay/DataDisplay.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import './DataDisplay.scss';
+import { subscribeAccelerometer, subscribeButtonPress } from './socket';
+
+class DataDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      accelerometerData: {
+        sensorId: '',
+        x: 0,
+        y: 0,
+        z: 0
+      },
+      buttonPressData: ''
+    }
+
+    subscribeAccelerometer((data) => {
+      // {"sensorId":"e96122f9c20c45528b7663f5dbafff95","x":"0.05","y":"-0.03","z":"1.00"}
+      this.setState({
+        accelerometerData: data
+      });
+    });
+
+    subscribeButtonPress((data) => {
+      // e96122f9c20c45528b7663f5dbafff95
+      this.setState({
+        buttonPressData: data
+      });
+    });
+  }
+
+  render() {
+    return(
+      <div>
+        <p>
+          accelerometerData: <br />
+          x: {this.state.accelerometerData.x} <br />
+          y: {this.state.accelerometerData.y} <br />
+          z: {this.state.accelerometerData.z}
+        </p>
+        <p>
+          buttonPressData: {this.state.buttonPressData}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default DataDisplay;
