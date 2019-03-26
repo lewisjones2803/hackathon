eb388749a96488051f7f17aa2eeb90d31858ccff
lewis.jones@dvla.gov.uk <lewis.jones@dvla.gov.uk>
2019-03-26 10:32:42 +0000

    created component class

---
+++ src/components/TireVis/TireVis.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TireVis.scss';
+
+class TireVis extends React.Component {
+  render() {
+    return(
+      <div></div>
+    );
+  }
+}
+
+export default TireVis;
